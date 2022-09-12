e0acf0341c74e8fbcaaeec56803dadf556c3c3cc
fedepaloz <palozzifederico@gmail.com>
2022-09-12 18:10:06 +0200

    customer added

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 include_once __DIR__ . "/models/product.php";
 include_once __DIR__ . "/models/food.php";
 include_once __DIR__ . "/models/customer.php";
+
+$cliente = new Customer ('Federico','Palozzi',true,'13/09/2022')
+
 ?>
 
+var_dump $
