b9282a18afb278b9f8e2604f77f2c2b15d36ca48
fedepaloz <palozzifederico@gmail.com>
2022-09-12 16:18:37 +0200

    class product

---
+++ product.php
@@ -0,0 +1,55 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $price;
+    public $description;
+
+
+    public function __construct($name, $price, $description)
+    {
+        $this->setName($name);
+        $this->setPrice($price);
+        $this ->setDescription($description);
+    }
+
+
+    //get name
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    //set name
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    //get price
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    //set price
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    //get description 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    //set description
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+
+    }
+}
