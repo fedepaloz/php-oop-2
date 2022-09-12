ad801bb7ac7e1c6b28c136b2da16852e1c62daa8
fedepaloz <palozzifederico@gmail.com>
2022-09-12 18:15:07 +0200

    to do in index.php

--- index.php
+++ index.php
@@ -3,8 +3,17 @@ include_once __DIR__ . "/models/product.php";
 include_once __DIR__ . "/models/food.php";
 include_once __DIR__ . "/models/customer.php";
 
-$cliente = new Customer ('Federico','Palozzi',true,'13/09/2022')
+$cliente = new Customer ('Federico','Palozzi',true,'13/09/2022');
+
+
+var_dump($cliente);
 
 ?>
 
-var_dump $
+
+<!-- mettere in maiuscolo le iniziali dei nomi delle cartelle 
+creare classe carta di credito perche non puo essere messa insieme al customer
+creare una classe extend di customer
+creare classe ordine
+creare classe carrello
+-->
